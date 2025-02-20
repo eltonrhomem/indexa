ab9a101e9d0efd4938d57b891235e62b2852a3ae
F2872541 Elton Rodrigues Homem <elton.homem@bb.com.br>
2025-02-20 08:34:32 -0300

    cabeçalho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from "./componentes/container/container.component";
+import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ContainerComponent, ContainerComponent],
+  imports: [CommonModule, RouterOutlet, ContainerComponent, CabecalhoComponent, CabecalhoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
