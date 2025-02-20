bbf6903e6abb22f134081151dfa660993f8fc5ff
F2872541 Elton Rodrigues Homem <elton.homem@bb.com.br>
2025-02-20 12:22:35 -0300

    fim do curso criando app web com Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,55 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from "./componentes/container/container.component";
 import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
+import { SeparadorComponent } from './componentes/separador/separador.component';
+import { ContatoComponent } from './componentes/contato/contato.component';
+import { FormsModule } from '@angular/forms';
 
+
+interface Contato {
+    id: number;
+    nome: string;
+    telefone: string;
+}
+
+import agenda from './agenda.json';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ContainerComponent, CabecalhoComponent, CabecalhoComponent],
+  imports: [
+            CommonModule, 
+            RouterOutlet, 
+            ContainerComponent, 
+            CabecalhoComponent, 
+            CabecalhoComponent, 
+            SeparadorComponent,
+            ContatoComponent,
+            FormsModule
+        ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'indexa';
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
+  contatos: Array<Contato> = agenda;
+  filtroPorTexto: string = '';
+
+  filtrarContatosPorTexto(): Array<Contato> {
+    if(!this.filtroPorTexto) {
+        return this.contatos;
+    }
+    return this.contatos.filter(contato => {
+        return contato.nome.toLowerCase().includes(this.filtroPorTexto.toLowerCase());
+    })
+  }
+
+  filtrarContatosPorLetraInicial(letra: string): Array<Contato> {
+    return this.filtrarContatosPorTexto().filter(contato => contato.nome.toLowerCase().startsWith(letra))
+    .sort((a, b) => a.nome.localeCompare(b.nome));
+  }
+
+  
+
+  
 }
